7af63e25a07fdc376c9eac0f347cc95567d6abf8
caubechankiu <trinhthevils@gmail.com>
2022-12-09 09:09:46 +0700

    update

--- config/simplesamlphp/authsources.php
+++ config/simplesamlphp/authsources.php
@@ -8,13 +8,18 @@ $config = array(
 
     'example-userpass' => array(
         'exampleauth:UserPass',
-        'vitt1:123456' => array(
+        'vitt:123456' => array(
             'uid' => array('1'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'vitt@yopmail.com',
+        ),
+        'vitt1:123456' => array(
+            'uid' => array('2'),
             'eduPersonAffiliation' => array('group1'),
             'email' => 'vitt1@yopmail.com',
         ),
         'vitt2:123456' => array(
-            'uid' => array('2'),
+            'uid' => array('3'),
             'eduPersonAffiliation' => array('group2'),
             'email' => 'vitt2@yopmail.com',
         ),
