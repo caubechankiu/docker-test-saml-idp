2badcd4a083b43933c0938d139f242955e18cb7b
caubechankiu <trinhthevils@gmail.com>
2022-12-13 17:57:05 +0700

    update

--- config/simplesamlphp/authsources.php
+++ config/simplesamlphp/authsources.php
@@ -8,20 +8,105 @@ $config = array(
 
     'example-userpass' => array(
         'exampleauth:UserPass',
-        'vitt@yopmail.com:123456' => array(
+        'sso1:12345678a' => array(
             'uid' => array('1'),
             'eduPersonAffiliation' => array('group1'),
-            'email' => 'vitt@yopmail.com',
+            'email' => 'sso1@zubairnews.com',
         ),
-        'vitt1:123456' => array(
+        'sso2:12345678a' => array(
             'uid' => array('2'),
             'eduPersonAffiliation' => array('group1'),
-            'email' => 'vitt1@yopmail.com',
+            'email' => 'sso2@zubairnews.com',
         ),
-        'vitt2:123456' => array(
+        'sso3:12345678a' => array(
             'uid' => array('3'),
-            'eduPersonAffiliation' => array('group2'),
-            'email' => 'vitt2@yopmail.com',
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso3@zubairnews.com',
+        ),
+        'sso4:12345678a' => array(
+            'uid' => array('4'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso4@zubairnews.com',
+        ),
+        'sso5:12345678a' => array(
+            'uid' => array('5'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso5@zubairnews.com',
+        ),
+        'sso6:12345678a' => array(
+            'uid' => array('6'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso6@zubairnews.com',
+        ),
+        'sso7:12345678a' => array(
+            'uid' => array('7'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso7@zubairnews.com',
+        ),
+        'sso8:12345678a' => array(
+            'uid' => array('8'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso8@zubairnews.com',
+        ),
+        'sso9:12345678a' => array(
+            'uid' => array('9'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso9@zubairnews.com',
+        ),
+        'sso10:12345678a' => array(
+            'uid' => array('10'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso10@zubairnews.com',
+        ),
+        'sso11:12345678a' => array(
+            'uid' => array('11'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso11@zubairnews.com',
+        ),
+        'sso12:12345678a' => array(
+            'uid' => array('12'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso12@zubairnews.com',
+        ),
+        'sso13:12345678a' => array(
+            'uid' => array('13'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso13@zubairnews.com',
+        ),
+        'sso14:12345678a' => array(
+            'uid' => array('14'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso14@zubairnews.com',
+        ),
+        'sso15:12345678a' => array(
+            'uid' => array('15'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso15@zubairnews.com',
+        ),
+        'sso16:12345678a' => array(
+            'uid' => array('16'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso16@zubairnews.com',
+        ),
+        'sso17:12345678a' => array(
+            'uid' => array('17'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso17@zubairnews.com',
+        ),
+        'sso18:12345678a' => array(
+            'uid' => array('18'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso18@zubairnews.com',
+        ),
+        'sso19:12345678a' => array(
+            'uid' => array('19'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso19@zubairnews.com',
+        ),
+        'sso20:12345678a' => array(
+            'uid' => array('20'),
+            'eduPersonAffiliation' => array('group1'),
+            'email' => 'sso20@zubairnews.com',
         ),
     ),
 
